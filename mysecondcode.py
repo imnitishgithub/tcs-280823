bed536850d07e87650bd2b1841a9ff119c2fe54d
imnitishgithub <abc@gmail.com>
2023-08-31 11:21:30 +0530

    dded all files

---
+++ mysecondcode.py
@@ -0,0 +1,11 @@
+This is my second code.
+
+It will be used in Codepipeline.
+
+We will use the same with Azure Cloud also.
+
+We can setup AKS cluster with SQL DB and Vault for keeping the code.
+
+It should run our Azure Pipeline.
+
+
