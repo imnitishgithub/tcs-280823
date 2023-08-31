e261b45ee336ddd5f8a9b3675dc3eef903de943e
imnitishgithub <abc@gmail.com>
2023-08-31 11:05:09 +0530

    adding modifie myfirstcode.py file

--- myfirstcode.py
+++ myfirstcode.py
@@ -4,4 +4,10 @@ We will going to use the same in our AWS Lambda function.
 
 Our Jenkins job will trigger this Lambda code using CodeDeploy service.
 
+Post commit adding this message.
+
+Let's see what will happen by running git diff command.
+
+
+
 
